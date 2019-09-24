caf0df0fab6f52e44a45209a7b712cba0a7c6e5a
Mohamed El Housseine <elh.mohamed@gmail.com>
2019-09-25 01:22:46 +0200

    minimal running machine

--- hw/arm/stm32_emh.c
+++ hw/arm/stm32_emh.c
@@ -2,20 +2,70 @@
  */
 
 #include "qemu/osdep.h" //os dependency is the most important include
+#include "qapi/error.h"
 #include "hw/boards.h"  //defines the machine creation macros
+#include "hw/sysbus.h"
+#include "hw/qdev-properties.h"
+#include "target/arm/cpu.h" //defines ARM_CPU_TYPE_NAME
+#include "hw/arm/boot.h"	//arm_load_kernel
+#include "exec/address-spaces.h" //get_system_memory
 
+
+#include "hw/char/pl011.h"
+#include "sysemu/sysemu.h"
+
+static struct arm_boot_info versatile_binfo;
 
 static void stm32_emh_init(MachineState *machine)
 {
+	DeviceState * dev, *sysctl;
+
+    printf("!!!! %s\n",  machine->cpu_type);
+    printf("!!!! %s\n",  machine->kernel_filename);
+    printf("!!!! %s\n",  machine->kernel_cmdline);
+    printf("!!!! %s\n",  machine->initrd_filename);
+    printf("!!!! %ld\n", machine->ram_size);
+
+
+    //create cpu
+    Object *cpuobj = object_new(machine->cpu_type);
+    object_property_set_bool(cpuobj, true, "realized", &error_fatal);
+    ARMCPU *cpu = ARM_CPU(cpuobj);
+
+    //create ram
+    MemoryRegion *address_space_mem = get_system_memory();
+    MemoryRegion *ram = g_new(MemoryRegion, 1);
+    memory_region_allocate_system_memory(ram, NULL, "emh.ram", machine->ram_size);
+    memory_region_add_subregion(address_space_mem, 0, ram);
+    //
+
+    sysctl = qdev_create(NULL, "realview_sysctl");
+    qdev_init_nofail(sysctl);
+    sysbus_mmio_map(SYS_BUS_DEVICE(sysctl), 0, 0x10000000);
+
+
+    dev = sysbus_create_varargs("pl190", 0x10140000,
+                                qdev_get_gpio_in(DEVICE(cpu), ARM_CPU_IRQ),
+                                qdev_get_gpio_in(DEVICE(cpu), ARM_CPU_FIQ),
+                                NULL);
+
+    qemu_irq pic[32];
+    for (int n = 0; n < 32; n++) {
+         pic[n] = qdev_get_gpio_in(dev, n);
+     }
+    pl011_create(0x101f1000, pic[12], serial_hd(0));
+
+    versatile_binfo.ram_size = machine->ram_size;
+    arm_load_kernel(cpu, machine, &versatile_binfo);
 
 }
 
-static void stm32_emh_class_init(ObjectClass *oc, void *data)
-{
+static void stm32_emh_class_init(ObjectClass *oc, void *data) {
     MachineClass *mc = MACHINE_CLASS(oc);
-
     mc->desc = "STM32 emh";
     mc->init = stm32_emh_init;
+//    mc->default_cpu_type = ARM_CPU_TYPE_NAME("cortex-m4");
+    mc->default_cpu_type = ARM_CPU_TYPE_NAME("arm926");
 }
 
 static const TypeInfo stm32_emh_type = {
@@ -24,8 +74,7 @@ static const TypeInfo stm32_emh_type = {
     .class_init = stm32_emh_class_init,
 };
 
-static void stm32_emh_machine_init(void)
-{
+static void stm32_emh_machine_init(void) {
     type_register_static(&stm32_emh_type);
 }
 
